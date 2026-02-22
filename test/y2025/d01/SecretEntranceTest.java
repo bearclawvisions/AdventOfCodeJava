3c6ef4c798be27108b5dc802bfb58769c4e9e7f5
bearclawvisions <113764031+bearclawvisions@users.noreply.github.com>
2026-02-22 13:43:45 +0100

    add another test for part 2

--- test/y2025/d01/SecretEntranceTest.java
+++ test/y2025/d01/SecretEntranceTest.java
@@ -9,6 +9,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class SecretEntranceTest {
 
     private static final List<String> TEST_INPUT = List.of("L68", "L30", "R48", "L5", "R60", "L55", "L1", "L99", "R14", "L82");
+    private static final List<String> TEST_INPUT_PART_2 = List.of("R1000");
 
     @Test
     public void testPart1() {
@@ -20,5 +21,11 @@ public class SecretEntranceTest {
     public void testPart2() {
         int result = SecretEntrance.part2(TEST_INPUT);
         assertEquals(6, result);
+    }
+
+    @Test
+    public void testPart2SingleStep() {
+        int result = SecretEntrance.part2(TEST_INPUT_PART_2);
+        assertEquals(10, result);
     }
 }
